62e5dbb3d8a55f25ab3747bce7a1100e974d8601
Dinislam Nizamov <58144991+1majic@users.noreply.github.com>
2022-03-11 12:21:33 +0500

    Create 4.sol

---
+++ 4/4.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Storage {
+
+    uint256 max;
+    uint[] memory GoodNumbers;
+    bool Flag;
+
+    function result(uint _temp) public returns(uint256) {
+        for(i=GoodNumbers.lengt;i>0;i--) {
+            for(j=999; j>100; j--) {
+                if (i % j % 1 == 0) {
+                    break
+                }
+            return i
+            }
+        }
+    }
+}
